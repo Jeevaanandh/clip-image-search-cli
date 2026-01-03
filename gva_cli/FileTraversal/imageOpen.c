f79c515cc109f17019d2de006fb73c5e64e8a63f
jeevaanandh <anandhjeeva215@gmail.com>
2026-01-04 00:04:51 +0530

    Made the opne command work for Linux, Mac and Windows

--- gva_cli/FileTraversal/imageOpen.c
+++ gva_cli/FileTraversal/imageOpen.c
@@ -3,8 +3,22 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]) {
+
     char command[512];
-    snprintf(command, sizeof(command), "open \"%s\"", argv[1]);
-    system(command);
+    #ifdef __APPLE__
+        snprintf(command, sizeof(command), "open \"%s\"", argv[1]);
+        system(command);
+
+    #elif _WIN32
+        snprintf(command, sizeof(command), "start \"%s\"", argv[1]);
+        system(command);
+
+    #elif __elif__
+        snprintf(command, sizeof(command), "xdg-open \"%s\"", argv[1]);
+        system(command);
+
+
+    #endif
+
     return 0;
 }
